20ac015bacc3aae8b50eb0d7470bd0215ddfce70
ychi0071 <ychi0071@student.monash.edu>
2025-05-15 18:42:12 +1000

    Change of setting gradle kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ksp)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
     id("com.google.gms.google-services") version "4.4.2" apply false
+
 
 }
 
